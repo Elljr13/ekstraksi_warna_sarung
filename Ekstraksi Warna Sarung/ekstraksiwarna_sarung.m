65f87ebc0f69dd06b4724815f768b643a362aa02
Ell_Jr <104006626+Elljr13@users.noreply.github.com>
2022-05-28 21:57:34 +0700

    Add files via upload

---
+++ Ekstraksi Warna Sarung/ekstraksiwarna_sarung.m
@@ -0,0 +1,200 @@
+function varargout = ekstraksiwarna_sarung(varargin)
+% EKSTRAKSIWARNA_SARUNG MATLAB code for ekstraksiwarna_sarung.fig
+%      EKSTRAKSIWARNA_SARUNG, by itself, creates a new EKSTRAKSIWARNA_SARUNG or raises the existing
+%      singleton*.
+%
+%      H = EKSTRAKSIWARNA_SARUNG returns the handle to a new EKSTRAKSIWARNA_SARUNG or the handle to
+%      the existing singleton*.
+%
+%      EKSTRAKSIWARNA_SARUNG('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in EKSTRAKSIWARNA_SARUNG.M with the given input arguments.
+%
+%      EKSTRAKSIWARNA_SARUNG('Property','Value',...) creates a new EKSTRAKSIWARNA_SARUNG or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before ekstraksiwarna_sarung_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to ekstraksiwarna_sarung_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help ekstraksiwarna_sarung
+
+% Last Modified by GUIDE v2.5 28-May-2022 21:09:58
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @ekstraksiwarna_sarung_OpeningFcn, ...
+                   'gui_OutputFcn',  @ekstraksiwarna_sarung_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before ekstraksiwarna_sarung is made visible.
+function ekstraksiwarna_sarung_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to ekstraksiwarna_sarung (see VARARGIN)
+
+% Choose default command line output for ekstraksiwarna_sarung
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes ekstraksiwarna_sarung wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = ekstraksiwarna_sarung_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+open=guidata(gcbo);
+[namafile]=uigetfile({'*.jpg;*.jpeg;*.webp;*.bmp;*.tif'},'openimage');
+I=imread(namafile);
+set(open.figure1,'CurrentAxes',open.axes1);
+set(imagesc(I));colormap('gray');
+set(open.axes1,'Userdata',I);
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+open=guidata(gcbo);
+E=get(open.axes1,'Userdata');
+fR=E(:,:,1);
+fG=E(:,:,2);
+fB=E(:,:,3);
+fr=mean(mean(fR));
+fb=mean(mean(fG));
+fc=mean(mean(fB));
+frr=fr/255;
+fbb=fb/255;
+fcc=fc/255;
+total=(frr+fbb+fcc)/3;
+h=(frr+fbb+fcc);
+set(handles.edit1,'String',frr)
+set(handles.edit2,'String',fbb)
+set(handles.edit3,'String',fcc)
+set(handles.edit4,'String',h)
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit3_Callback(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit3 as text
+%        str2double(get(hObject,'String')) returns contents of edit3 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit4_Callback(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit4 as text
+%        str2double(get(hObject,'String')) returns contents of edit4 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit4_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
